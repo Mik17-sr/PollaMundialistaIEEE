d8358e95f9901de829f1511e2b3dadc73b1671cd
Mik17-sr <miguelangelsierralarrota@hotmail.com>
2026-04-07 23:53:03 -0500

    App

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,10 +1,17 @@
 <?php
-$host = "localhost";
+/*$host = "localhost";
 $user = "root";
 $pass = "admin7942_";
 $db = "polla_db";
+*/
 
-$conn = new mysqli($host, $user, $pass, $db);
+$host = "maglev.proxy.rlwy.net";
+$port = 55559;
+$user = "root"; 
+$pass = "AUWnsHGUiRFPssoFbCHzXeyltpLkkiWa";
+$db = "polla_db";
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
